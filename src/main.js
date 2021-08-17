3480d1ddc53607dd132f5409b09ad3b4bc0483d6
Cris GC <cris.gcc@outlook.com>
2021-08-17 12:36:55 -0500

    Configuracion basica

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
+//FONT AWESOME
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { library, config } from '@fortawesome/fontawesome-svg-core'
+  import { fab } from '@fortawesome/free-brands-svg-icons'
+  import { far } from '@fortawesome/free-regular-svg-icons'
+  import { fas } from '@fortawesome/free-solid-svg-icons'
+  library.add(fab)
+  library.add(far)
+  library.add(fas)
+//COMPONENTS 
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+//
+window.axios = require('axios');
+//
 Vue.config.productionTip = false
 
 new Vue({
